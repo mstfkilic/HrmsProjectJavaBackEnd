9c3bed401dabc9564674b361be3ed976c0f1de90
= <=>
2021-11-02 00:45:13 +0300

    First Commit

---
+++ hrms/src/main/java/kodlama/io/hrms/business/concretes/UserManager.java
@@ -0,0 +1,32 @@
+package kodlama.io.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlama.io.hrms.business.abstracts.UserService;
+import kodlama.io.hrms.dataAccess.abstracts.UserDao;
+import kodlama.io.hrms.entities.concretes.User;
+
+@Service
+public class UserManager implements UserService{
+
+	private UserDao userDao;
+	
+	
+	@Autowired
+	public UserManager(UserDao userDao) {
+		super();
+		this.userDao = userDao;
+	}
+
+
+	@Override
+	public List<User> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
